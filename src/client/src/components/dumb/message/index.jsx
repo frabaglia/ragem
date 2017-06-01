d18077ca82aaa1798d804f039d70896d76878eb1
Franco Rabaglia <francoo.hd@gmail.com>
2017-06-01 00:01:40 -0300

    Adding left and right behaviour

--- src/client/src/components/dumb/message/index.jsx
+++ src/client/src/components/dumb/message/index.jsx
@@ -1,27 +1,41 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import './style.css'
 class Message extends Component {
 
   render() {
-    var style
+    var buttonStyle
+    var rowStyle
 
     if (this.props.me === true) {
-      style = {
-        width:'100%',
+      buttonStyle = {
         backgroundColor: "red",
+        width: '30vw',
+        textAlign: 'left',
         padding: '2em',
         color: "white"
       }
+      rowStyle = {
+        alignItems: "flex-start",
+        justifyContent: 'flex-start'
+      }
     } else {
-      style = {
-        width:'100%',
+      buttonStyle = {
         backgroundColor: "blue",
+        width: '30vw',
+        textAlign: 'right',
         padding: '2em',
         color: "white"
+      }
+      rowStyle = {
+        alignItems: "flex-end",
+        justifyContent: 'flex-end'
       }
     }
     return (
-      <div style={style}>
-        {this.props.content}
+      <div className='row' style={rowStyle}>
+        <div style={buttonStyle}>
+          {this.props.content}
+        </div>
       </div>
     );
   }
