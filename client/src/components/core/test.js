80b4a3a69893cd7c2b602abc4da40c90c3bd59bc
Franco Rabaglia <francoo.hd@gmail.com>
2017-08-29 01:03:59 -0300

    Making dumb chat fully stateless.

--- client/src/components/core/test.js
+++ client/src/components/core/test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Core from '.';
 
-it('router policy renders without crashing', () => {
+it('router policy and core app renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Core />, div);
 });
--- client/src/components/dumb/chat-layout/index.jsx
+++ client/src/components/dumb/chat-layout/index.jsx
@@ -2,31 +2,29 @@ import React from 'react'
 import Message from '../message'
 
 export default function DumbChatLayout({error, messages, uid}) {
-  const messageLayout = (message, key) => {
-    if (uid === message.uid) {
-      return (<Message key={key.toString()} content={message.content} me={true}/>)
-    } else {
-      return (<Message key={key.toString()} content={message.content} me={false}/>)
-    }
+  if (messages) {
+    return (
+      <div>
+        <div>
+          {messages && messages.map((message, index) => {
+            if (uid === message.uid) {
+              return (<Message key={index.toString()} content={message.content} me={true}/>)
+            } else {
+              return (<Message key={index.toString()} content={message.content} me={false}/>)
+            }
+          })}
+        </div>
+      </div>
+    )
+  } else if (error) {
+    return (
+      <div>
+        {JSON.stringify(error)}
+      </div>
+    )
+  } else {
+    return (
+      <div>There is no messages...</div>
+    )
   }
-  const renderMessages = (messagesArray) => {
-    if (typeof messagesArray === 'undefined' || messagesArray == null || messagesArray == []) {
-      return (
-        <div></div>
-      )
-    }
-
-    var _array = new Array()
-    messagesArray.map((msg, index) => {
-      _array.push(messageLayout(msg, index))
-    })
-    return _array
-  }
-
-  return (
-    <div>
-      <h1>{JSON.stringify(error)}</h1>
-      <div>{renderMessages(messages)}</div>
-    </div>
-  )
 }
